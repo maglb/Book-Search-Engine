7a02d509116f62cfdf7ccc314b5f95e43e77a281
maglb <magali.lb21@gmail.com>
2024-04-03 21:36:51 -0600

    query me and mutation added

--- BookSearch/server/schemas/resolvers.js
+++ BookSearch/server/schemas/resolvers.js
@@ -1,23 +1,8 @@
-const { User, Book } = require("../models");
+const { User } = require("../models");
 const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
   Query: {
-    users: async () => {
-      return User.find().populate("savedBooks");
-    },
-
-    user: async (parent, { username }) => {
-      return User.findOne({ username }).populate("savedBooks");
-    },
-    thoughts: async (parent, { username }) => {
-      const params = username ? { username } : {};
-      return Book.find(params);
-    },
-    thought: async (parent, { bookId }) => {
-      return Book.findOne({ _id: bookId });
-    },
-
     me: async (parent, args, context) => {
       if (context.user) {
         return User.findOne({ _id: context.user._id }).populate("savedBooks");
@@ -27,13 +12,13 @@ const resolvers = {
   },
 
   Mutation: {
-    addUser: async (parent, { name, email, password }) => {
-      const user = await User.create({ name, email, password });
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
       const token = signToken(user);
 
       return { token, user };
     },
-    loginUser: async (parent, { email, password }) => {
+    login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
 
       if (!user) {
@@ -50,13 +35,12 @@ const resolvers = {
       return { token, user };
     },
 
-    saveBook: async (parent, { userId, skill }, context) => {
-      //TO REVIEW WITH WESLEY
+    saveBook: async (parent, { book }, context) => {
       if (context.user) {
-        return user.findOneAndUpdate(
+        return User.findOneAndUpdate(
           { _id: context.user._id },
           {
-            $addToSet: { savedBooks: book._id },
+            $push: { savedBooks: book },
           },
           {
             new: true,
@@ -69,13 +53,13 @@ const resolvers = {
     },
     removeBook: async (parent, { bookId }, context) => {
       if (context.user) {
-        const book = await Book.findOneAndDelete({
-          bookId: bookId,
-        });
 
-        await User.findOneAndUpdate(
+        return User.findOneAndUpdate(
           { _id: context.user._id },
-          { $pull: { savedBooks: bookId } }
+          { $pull: { savedBooks: { bookId } } },
+          {
+            new: true,
+          }
         );
       }
       throw AuthenticationError;
