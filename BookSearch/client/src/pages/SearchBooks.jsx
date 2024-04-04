d284b40299e50f29dea0d01e3908ae01f9d66e4d
maglb <magali.lb21@gmail.com>
2024-04-03 22:51:08 -0600

    Bug fixed

--- BookSearch/client/src/pages/SearchBooks.jsx
+++ BookSearch/client/src/pages/SearchBooks.jsx
@@ -76,7 +76,7 @@ const SearchBooks = () => {
 
     try {
       const { data } = await saveBook({
-        variables: { bookData: {...bookToSave} },
+        variables: { book: bookToSave },
       });
 
       // if book successfully saves to user's account, save book id to state
