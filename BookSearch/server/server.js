8595627bc6ebbade0090194af759f71e91495e55
maglb <magali.lb21@gmail.com>
2024-04-03 21:37:07 -0600

    contect middleware added

--- BookSearch/server/server.js
+++ BookSearch/server/server.js
@@ -5,6 +5,7 @@ const path = require('path');
 
 const { typeDefs, resolvers } = require("./schemas");
 const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
 // const routes = require('./routes');
 
 const app = express();
@@ -22,7 +23,7 @@ const startApolloServer = async () => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
-  app.use("/graphql", expressMiddleware(server));
+  app.use("/graphql", expressMiddleware(server, { context: authMiddleware}));
 
   // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === "production") {
